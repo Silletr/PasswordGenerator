6581dbb8d5f73ef988f22f9e9e3e0832c18824b2
Silletr <hristicenkoigor764@gmail.com>
2026-04-08 21:18:04 +0300

    [CHANGED FILE/DIR: .gitignore, src/passwordgen/core/generator.py, src/passwordgen/core/test.py] Deleted unnessary print() in generator.py, added test of password generation in test.py - successfully working, I'm glad that at least something working from first try :D [TIMESTAMP: 08/04 | 21:18 UTC+2]

--- src/passwordgen/core/generator.py
+++ src/passwordgen/core/generator.py
@@ -5,7 +5,6 @@ import string
 def generate_base(length=32):
     chars = string.ascii_letters + string.digits + string.punctuation
     password = "".join(secrets.choice(chars) for _ in range(length))
-    print(password)
     return password
 
 
