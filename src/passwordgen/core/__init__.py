3184dccadca565138937dc2f0fb4001d81001f3b
Silletr <hristicenkoigor764@gmail.com>
2026-04-09 15:50:20 +0300

    [GIT MERGE - SQUASH MODE] dded completely working CLI, added few commands for generate, save, and just Yes [TIMESTAMP: 09/04 | 15:50 UTC+2]

---
+++ src/passwordgen/core/__init__.py
@@ -0,0 +1,3 @@
+from .generator import generate_base
+
+generate_password = generate_base
--- src/passwordgen/core/test.py
+++ src/passwordgen/core/test.py
@@ -1,5 +1,5 @@
-from generator import generate_base
+from passwordgen import generate_password
 
 for i in range(6):  # because array's starting from zero. I need 5 passwords
-    print(f"Password number {i}, 16 symbols is:")
-    print(generate_base(16))
+    print(f"Password number {i} with 46 symbols is:")
+    print(generate_password(length=46))
